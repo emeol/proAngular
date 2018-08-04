9353e0caa5cfff4cb55e0da3f35b2377b1256616
unknown <eumeol@gmail.com>
2018-08-04 09:35:00 +0100

    starer kit

---
+++ compilertest.js
@@ -0,0 +1,11 @@
+"use strict";
+var Test = (function () {
+    function Test(message) {
+        this.message = message;
+    }
+    return Test;
+}());
+exports.Test = Test;
+// export class Test {
+//     constructor(public message:string){}
+// } 
---
+++ compilertest.ts
@@ -0,0 +1,7 @@
+export class Test {
+    constructor(public message: string) {}
+}
+
+// export class Test {
+//     constructor(public message:string){}
+// }
